d8e4fdce65c601f3b3aa49d82c2f0cebbf29161d
Cxva97 <cxva97@hotmail.com>
2025-11-10 20:22:02 -0500

    clase 9

---
+++ clase9.py
@@ -0,0 +1,23 @@
+#feedback
+# try except
+# es el manejo de errores
+
+"""
+NameError = Variable no declarada 
+TypeError = Operacion no posibles 
+FileNotFound = Archivono encontrado
+SyntaxErro = codigo mal escrito
+ValueError = Conversiones ej text a int 
+IndexError =  si trabajamos con listas o tuplas no hay la posicion
+KeyError = Si trabajamos con dict no hay key llave
+ZeroDivisionError = No se puede dividir para cero
+Exception
+"""
+
+
+try : 
+    print("hola")
+except  Exception as e:
+    print("Ha ocurrido un error: ", e) 
+finally:
+    print("ejecuta siempre")
---
+++ test.py
@@ -0,0 +1,20 @@
+"""
+NameError = Variable no declarada 
+TypeError = Operacion no posibles 
+FileNotFound = Archivono encontrado
+SyntaxErro = codigo mal escrito
+ValueError = Conversiones ej text a int 
+IndexError =  si trabajamos con listas o tuplas no hay la posicion
+KeyError = Si trabajamos con dict no hay key llave
+ZeroDivisionError = No se puede dividir para cero
+Exception
+"""
+
+lista= [1,2]
+print(lista[3])
+
+diccionario={
+    'nombre' : "cesar"
+}
+
+print(diccionario['edad'])
